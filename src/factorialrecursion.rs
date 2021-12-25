de90c3d4c1df1e7f09e3a876e758af3db429d93c
anurag2361 <sharmaanurag.236@gmail.com>
2021-12-25 19:37:21 +0530

    factorial recursion

---
+++ src/factorialrecursion.rs
@@ -0,0 +1,12 @@
+fn factorial(arg: i128) -> i128 {
+    if arg == 1 {
+        return arg;
+    }
+    return arg * factorial(arg - 1);
+}
+
+fn main() {
+    let num: i128 = 4;
+    let result = factorial(num);
+    println!("{}", result);
+}

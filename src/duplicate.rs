684b2b5f9fbb6a04e9d7ea80ae4e33a8d7fb1a34
anurag2361 <sharmaanurag.236@gmail.com>
2021-12-25 18:34:13 +0530

    find duplicate in array of number

---
+++ src/duplicate.rs
@@ -0,0 +1,22 @@
+// check if duplicate exist in array of number (by frquency counter)
+
+use std::collections::HashMap;
+
+fn are_there_duplicates(arr: Vec<i32>) -> bool {
+    let mut obj: HashMap<i32, usize> = HashMap::new();
+    for i in 0..arr.len() {
+        obj.entry(arr[i]).and_modify(|v| *v += 1).or_insert(1);
+    }
+    for (key, value) in &obj {
+        if obj.get(&key).unwrap() > &1 {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn main() {
+    let newvec = vec![1, 2, 3];
+    let result = are_there_duplicates(newvec);
+    println!("{:?}", result);
+}
